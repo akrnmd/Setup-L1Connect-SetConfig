b0c692af927f2b80e60264cf4442831100ba4777
yuki-s-y <yuki_yamamoto@adoc.co.jp>
2018-07-26 19:13:24 +0900

    Add Configuration input function.

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 from cloudshell.workflow.orchestration.sandbox import Sandbox
 from cloudshell.workflow.orchestration.setup.default_setup_orchestrator import DefaultSetupWorkflow
 import Proc_L1ConnectionController as L1ConnectionController
+from Work_Config import WorkConfig
 
 
 def main():
@@ -15,6 +16,9 @@ def main():
 
 # Set Config
 # insert code here
+    work_config = WorkConfig()
+    work_config.input_config_all("Config Path")
+
 
 # Entry Point
 if __name__ == "__main__":
