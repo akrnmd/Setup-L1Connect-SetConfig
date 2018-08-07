a503c593ea27183d5362320d71956ec44d4a7e54
AD-PC-RT-00259\kiarima <kin_arima@adoc.co.jp>
2018-08-07 17:39:12 +0900

    re write L1CC function to class

--- Proc_L1ConnectionController.py
+++ Proc_L1ConnectionController.py
@@ -1,30 +1,26 @@
 import cloudshell.helpers.scripts.cloudshell_scripts_helpers as helpers
-
 # How to use this file
+#
+# from Proc_L1ConnectionController import L1ConnectionController
+# L1CC = L1ConnectionController()
+# L1CC.ChangeStateOfAllL1Routes("Connect")
+class L1ConnectionController:
+    def __init__(self):
+        self.reservation_id = helpers.get_reservation_context_details().id
+        self.session = helpers.get_api_session()
+        self.routes = self.session.GetReservationDetails(self.reservation_id).ReservationDescription.RequestedRoutesInfo
 
-# import Proc_L1ConnectionController as L1ConnectionController
-# L1ConnectionController.ChangeStateOfAllL1Routes("Connect")
-# or
-# L1ConnectionController.ChangeStateOfAllL1Routes("Disconnect")
+    def ChangeStateOfAllL1Routes(self, TargetState):
+        for route in self.routes:
+            endpoints = []
 
-def ChangeStateOfAllL1Routes(TargetState):
-    reservation_id = helpers.get_reservation_context_details().id
-
-    session = helpers.get_api_session()
-    resources = session.GetReservationDetails(reservation_id).ReservationDescription
-
-    routes = session.GetReservationDetails(reservation_id).ReservationDescription.RequestedRoutesInfo
-
-    for route in routes:
-        endpoints = []
-
-        routeType = route.RouteType
-        endpoints.append(route.Source)
-        endpoints.append(route.Target)
-        if TargetState == "Connect":
-            session.WriteMessageToReservationOutput(reservation_id, "Connect route: " + route.Source + " to:" + route.Target)
-            session.ConnectRoutesInReservation(reservation_id, endpoints, routeType)
-        if TargetState == "Disconnect":
-            session.WriteMessageToReservationOutput(reservation_id,
-                                                    "DisConnect route: " + route.Source + " to:" + route.Target)
-            session.DisconnectRoutesInReservation(reservation_id, endpoints)
+            routeType = route.RouteType
+            endpoints.append(route.Source)
+            endpoints.append(route.Target)
+            if TargetState == "Connect":
+                self.session.WriteMessageToReservationOutput(self.reservation_id, "Connect route: " + route.Source + " to:" + route.Target)
+                self.session.ConnectRoutesInReservation(self.reservation_id, endpoints, routeType)
+            if TargetState == "Disconnect":
+                self.session.WriteMessageToReservationOutput(self.reservation_id,
+                                                        "DisConnect route: " + route.Source + " to:" + route.Target)
+                self.session.DisconnectRoutesInReservation(self.reservation_id, endpoints)
